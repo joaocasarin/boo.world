b8e2a72732eae22b1b56cc8ba586a6bfb25db809
João Casarin <joaocasarin@mbp.local>
2024-03-21 15:21:08 -0300

    feat: add cors and helmet middlewares

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict';
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
+const { default: helmet } = require('helmet');
 
 const profileRoute = require('./routes/profile');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type']
+}));
+app.use(helmet());
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
