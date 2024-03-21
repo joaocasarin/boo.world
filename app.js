39970ca5a995f2d40650415e8a47a38a3277fe79
João Casarin <joaocasarin@mbp.local>
2024-03-21 15:10:30 -0300

    refactor: import routes at the beginning of app file

--- app.js
+++ app.js
@@ -3,10 +3,15 @@
 const express = require('express');
 const app = express();
 
+const profileRoute = require('./routes/profile');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // routes
-app.use('/', require('./routes/profile')());
+app.use('/', profileRoute);
 
 module.exports = app;
